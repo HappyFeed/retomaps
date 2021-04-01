ba6453c54f697add15df619bfaabb7bce8d8d1c5
HappyFeed <alejomam2011@gmail.com>
2021-04-01 15:01:35 -0500

    ahi humildemente

---
+++ app/src/main/java/app/moviles/retomaps/MenuFragment.java
@@ -0,0 +1,38 @@
+package app.moviles.retomaps;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MenuFragment extends Fragment {
+
+    //state
+
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+    public static MenuFragment newInstance(String param1, String param2) {
+        MenuFragment fragment = new MenuFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+}
